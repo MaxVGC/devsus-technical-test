a4fe841dc88a13175838fe4c05793b03c153f97b
MaxVGC <carlos.andres12390@live.com>
2025-10-22 01:28:17 -0500

    feat: initialize account-movements-microservice with Maven and Spring Boot setup

---
+++ account-movements-microservice/src/main/java/com/devsu/account_movements_microservice/AccountMovementsMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.devsu.account_movements_microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountMovementsMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountMovementsMicroserviceApplication.class, args);
+	}
+
+}
---
+++ account-movements-microservice/src/test/java/com/devsu/account_movements_microservice/AccountMovementsMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsu.account_movements_microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountMovementsMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
