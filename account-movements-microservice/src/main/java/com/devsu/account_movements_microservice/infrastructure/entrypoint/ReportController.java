da690156ff35aa09f455967b187bd12d7d391c14
MaxVGC <carlos.andres12390@live.com>
2025-10-22 23:23:08 -0500

    feat: Implement report generation endpoint in ReportController

--- account-movements-microservice/src/main/java/com/devsu/account_movements_microservice/infrastructure/entrypoint/ReportController.java
+++ account-movements-microservice/src/main/java/com/devsu/account_movements_microservice/infrastructure/entrypoint/ReportController.java
@@ -1,13 +1,27 @@
 package com.devsu.account_movements_microservice.infrastructure.entrypoint;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.devsu.account_movements_microservice.application.dtos.in.QueryReportDTO;
+import com.devsu.account_movements_microservice.application.dtos.out.ReportResponseDTO;
+import com.devsu.account_movements_microservice.application.services.IReportService;
+
 import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Mono;
 
 @RestController
-@RequestMapping("/api/v1/movements")
+@RequestMapping("/api/v1/reports")
 @RequiredArgsConstructor
 public class ReportController {
+
+    private final IReportService reportService;
+
+    @GetMapping
+    public ResponseEntity<Mono<ReportResponseDTO>> generateReport(QueryReportDTO query) {
+        return ResponseEntity.ok(reportService.generateReport(query));
+    }
     
 }
